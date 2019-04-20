fd48a55d3bfccf8f3bc92d87cd9bce83132f43b2
Tom Kain <tom.kain@gmail.com>
2019-04-19 21:28:12 -0700

    Retrieving latest standings, join with picks

---
+++ 2019_mlb/R/standings.R
@@ -0,0 +1,31 @@
+library(baseballr)
+library(tidyverse)
+library(janitor)
+library(magrittr)
+
+get_standings <- function(date=Sys.Date(), from=FALSE) {
+  divisions <- c(
+    "NL East",
+    "NL Central",
+    "NL West",
+    "AL East",
+    "AL Central",
+    "AL West"
+  )
+  
+  purrr::map(.x = divisions, 
+             .f = get_standings_division, 
+             date = Sys.Date(),
+             from = from) %>% 
+    bind_rows()
+}
+
+get_standings_division <- function(date, from, division) {
+  standings_on_date_bref(date = date, 
+                         from = from, 
+                         division = division) %>% 
+    extract2(1) %>% 
+    clean_names() %>% 
+    as_tibble() %>% 
+    mutate(division = division)
+}
